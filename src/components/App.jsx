f71a6f132c5e0b0c49ee648fb1ccacf3602fd84c
Daniel Lee <daniejlee@gmail.com>
2021-03-19 20:28:03 -0700

    setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,70 @@
 import React from 'react';
+import Background from './Background'
+import Header from './Header'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 'industries'
+      page: 'industries',
+      type: '',
+      pageContent: null,
+      background: ''
     };
 
-    this.setPage = this.setPage.bind(this)
+    this.changeContent = this.changeContent.bind(this)
   }
 
-  setPage(name){
-    this.setState({
-      page: name
-    })
+  componentDidMount(){
+    this.changeContent('services')
   }
+
+  //name parameter is the id/key? of the menu button that is clicked
+  changeContent(pageName){
+
+    fetch('./content.json')
+    .then(response => response.json())
+    .then(data => {
+      let pages = data.pages
+      let pageData = pages.find(element => element.slug === pageName)
+      console.log(pageData)
+
+      this.setState({
+        page: pageName,
+        pageContent: pageData
+      })
+
+
+    })
+
+  }
+
+
 
   render(){
-    return
+    let currentPage;
+    switch (this.state.page){
+      case 'industries':
+        // currentPage = (
+        //   <div></div>
+        // )'
+        console.log('industries')
+        break;
+
+      case 'services':
+        console.log('services');
+          break;
+
+      default:
+        return null;
+    }
+  return (
+  <div>
+    <Background backgroundImg={this.state.img}/>
+    <Header />
+
+  </div>
+  )
   }
 
 }
